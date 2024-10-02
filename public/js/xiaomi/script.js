19890ed95fab6d03a3a1397436b13b91d9111f6a
Solopov Daniil <d.solopov@agima.ru>
2024-10-03 01:44:29 +0300

    Вёрстка ч2

--- public/js/xiaomi/script.js
+++ public/js/xiaomi/script.js
@@ -1,11 +1,13 @@
 $(function (){
 
-    $('.bike__slider').slick({
+    $('.bike__slider, .slider__items').slick({
         dots: true,
         arrows: false,
         fade: true,
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+
 
 });
